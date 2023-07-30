4472e3ab873b96cc942dc7fbfe516282b9dfc681
Johannes Fritsch <fritsch@zoom7.de>
2023-07-30 20:37:17 +0200

    Initial commit

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+import withMarkdoc from '@markdoc/next.js'
+
+import withSearch from './src/markdoc/search.mjs'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  pageExtensions: ['js', 'jsx', 'md'],
+  experimental: {
+    scrollRestoration: true,
+  },
+}
+
+export default withSearch(
+  withMarkdoc({ schemaPath: './src/markdoc' })(nextConfig)
+)
